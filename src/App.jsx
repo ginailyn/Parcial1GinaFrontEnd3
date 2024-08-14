689b5a332831483eea44a26cda6a6b510b9466c6
= <=>
2024-08-13 21:08:01 -0500

    Subo Esqueleto del Parcial

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import Card from "./Card";
+import './App.css'
+
+function App() {
+ //  const [count, setCount] = useState(0)
+
+  return (
+
+    <div className="App">
+         <h1>Carga de estudiantes</h1>
+         <form></form>
+         <Card />
+    </div>
+
+
+  )
+}
+
+export default App;
